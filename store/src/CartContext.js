f601632def85e851dd02520c3b35fc1d81b3467d
Joy Evans <joyevans@me.com>
2022-10-26 09:44:58 -0500

    added cart functions

--- store/src/CartContext.js
+++ store/src/CartContext.js
@@ -12,13 +12,58 @@ export const CartContext = createContext({
 
 export function CartProvider({children}) {
     const [cartProducts, setCartProducts] = useState([]);
+
+    function getProductQuantity(id) {
+        const quantity = cartProducts.find(product => product.id === id)?.quantity;
+
+        if (quantity === undefined){
+            return 0;
+        }
+
+        return quantity;
+    }
+
+    function addOneToCart(id) {
+        const quantity = getProductQuantity(id);
+
+        if (quantity === 0 ) {
+            setCartProducts(
+                [
+                    ...cartProducts,
+                    {
+                        id: id,
+                        quantity: 1
+                    }
+                ]
+            )
+        } else {
+            setCartProducts(
+                cartProducts.map(
+                    product =>
+                    product.id === id
+                    ? {...product, quantity: product.quantity + 1}
+                    : product
+                )
+            )
+        }
+    }
+
+    function deleteFromCart(id) {
+        
+    }
     
     const contextValue = {
-        items: [],
+        items: cartProducts,
         getProductQuantity,
         addOneToCart,
         removeOneFromCart,
         deleteFromCart,
         getTotalCost
     }
+
+    return (
+        <CartContext.Provider value={contextValue}>
+            {children}
+        </CartContext.Provider>
+    )
 }
