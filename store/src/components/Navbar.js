23b646cd08419e622309db4c6653b6d0acb0b956
Joy Evans <joyevans@me.com>
2022-10-30 08:56:14 -0500

    modified data shown in cart

--- store/src/components/Navbar.js
+++ store/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import {Button, Container, Navbar, Modal} from 'react-bootstrap'
 import {useState, useContext} from 'react';
 import { CartContext } from '../CartContext';
+import CartProduct from './CartProduct';
 
 function NavbarComponent() {
     const cart = useContext(CartContext);
@@ -28,9 +29,13 @@ function NavbarComponent() {
                     {productsCount > 0 ?
                         <>
                             <p>Items in your cart:</p>
-                            {}
+                            {cart.items.map((currentProduct, idx) => (
+                                <CartProduct key={idx} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
+                            ))}
 
                             <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
+
+                            <Button variant='success'>Purchase items!</Button>
                         </>
                     :
                         <h1>There are no items in cart!</h1>
